a18f6228d7ac8919cd3841def38ce7e95d5c57ff
Tanner Rigby <tanner.rigby@canopytax.com>
2021-11-24 16:24:12 -0700

    adding in kids names

---
+++ src/Home.helper.js
@@ -0,0 +1,86 @@
+const kira = require("./assets/kira.png");
+const ben = require("./assets/ben.png");
+const ashley = require("./assets/ash.png");
+const mike = require("./assets/mike.png");
+const david = require("./assets/david.png");
+const sarah = require("./assets/sarah.png");
+const tanner = require("./assets/tan.png");
+const kylee = require("./assets/ky.png");
+const reese = require("./assets/reese.png");
+const riley = require("./assets/riley.png");
+const rowen = require("./assets/rowen.png");
+const max = require("./assets/max.png");
+const gavin = require("./assets/gavin.png");
+const bailey = require("./assets/bailey.png");
+const leah = require("./assets/leah.png");
+const oakley = require("./assets/oakley.png");
+
+export const adultMatchups = [
+  {
+    giveFrom: { name: "kira", img: kira },
+    giveTo: { name: "sarah", img: sarah },
+  },
+  {
+    giveFrom: { name: "ben", img: ben },
+    giveTo: { name: "ashley", img: ashley },
+  },
+  {
+    giveFrom: { name: "ashley", img: ashley },
+    giveTo: { name: "kylee", img: kylee },
+  },
+  {
+    giveFrom: { name: "mike", img: mike },
+    giveTo: { name: "david", img: david },
+  },
+  {
+    giveFrom: { name: "david", img: david },
+    giveTo: { name: "tanner", img: tanner },
+  },
+  {
+    giveFrom: { name: "sarah", img: sarah },
+    giveTo: { name: "kira", img: kira },
+  },
+  {
+    giveFrom: { name: "tanner", img: tanner },
+    giveTo: { name: "ben", img: ben },
+  },
+  {
+    giveFrom: { name: "kylee", img: kylee },
+    giveTo: { name: "mike", img: mike },
+  },
+];
+
+export const kidMatchups = [
+  {
+    giveFrom: { name: "reese", img: reese },
+    giveTo: { name: "max", img: max },
+  },
+  {
+    giveFrom: { name: "riley", img: riley },
+    giveTo: { name: "leah", img: leah },
+  },
+  {
+    giveFrom: { name: "rowen", img: rowen },
+    giveTo: { name: "bailey", img: bailey },
+  },
+  {
+    giveFrom: { name: "max", img: max },
+    giveTo: { name: "rowen", img: rowen },
+  },
+  {
+    giveFrom: { name: "gavin", img: gavin },
+    giveTo: { name: "reese", img: reese },
+  },
+  {
+    giveFrom: { name: "bailey", img: bailey },
+    giveTo: { name: "oakley", img: oakley },
+  },
+  {
+    giveFrom: { name: "leah", img: leah },
+    giveTo: { name: "riley", img: riley },
+  },
+  {
+    giveFrom: { name: "oakley", img: oakley },
+    giveTo: { name: "gavin", img: gavin },
+  },
+];
--- src/Home.js
+++ src/Home.js
@@ -1,119 +1,45 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import "./Home.css";
+import { adultMatchups, kidMatchups } from "./Home.helper";
 
-const kira = require("./assets/kira.png");
-const ben = require("./assets/ben.png");
-const ash = require("./assets/ash.png");
-const mike = require("./assets/mike.png");
-const david = require("./assets/david.png");
-const sarah = require("./assets/sarah.png");
-const tanner = require("./assets/tan.png");
-const kylee = require("./assets/ky.png");
+export default function App() {
+  const [listSelected, setListSelected] = useState("adults");
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="list-header">Christmas Assignments</div>
-        <div className="list">
-          <div className="list-item">
-            <img src={kira} alt="kira" height="100" width="100" />
-            <span
-              style={{ fontSize: "40px" }}
-              role="img"
-              aria-label="gives gift to"
-            >
-              🎁 →
-            </span>
-            <img src={sarah} alt="sarah" height="100" width="100" />
-          </div>
-          <div className="list-item">
-            <img src={ben} alt="ben" height="100" width="100" />
-            <span
-              style={{ fontSize: "40px" }}
-              role="img"
-              aria-label="gives gift to"
-            >
-              🎁→
-            </span>
-            <img src={ash} alt="ash" height="100" width="100" />
-          </div>
-          <div className="list-item">
-            <img src={ash} alt="ash" height="100" width="100" />
-            <span
-              style={{ fontSize: "40px" }}
-              role="img"
-              aria-label="gives gift to"
-            >
-              🎁→
-            </span>
-            <img src={kylee} alt="kylee" height="100" width="100" />
-          </div>
-
-          <div className="list-item">
-            <img src={mike} alt="mike" height="100" width="100" />
-            <span
-              style={{ fontSize: "40px" }}
-              role="img"
-              aria-label="gives gift to"
-            >
-              🎁→
-            </span>
-            <img src={david} alt="david" height="100" width="100" />
-          </div>
-
-          <div className="list-item">
-            <img src={david} alt="david" height="100" width="100" />
-            <span
-              style={{ fontSize: "40px" }}
-              role="img"
-              aria-label="gives gift to"
-            >
-              🎁→
-            </span>
-            <img src={tanner} alt="tanner" height="100" width="100" />
-          </div>
-
-          <div className="list-item">
-            <img src={sarah} alt="sarah" height="100" width="100" />
-            <span
-              style={{ fontSize: "40px" }}
-              role="img"
-              aria-label="gives gift to"
-            >
-              🎁→
-            </span>
-            <img src={kira} alt="kira" height="100" width="100" />
-          </div>
-
-          <div className="list-item">
-            <img src={tanner} alt="tanner" height="100" width="100" />
-            <span
-              style={{ fontSize: "40px" }}
-              role="img"
-              aria-label="gives gift to"
-            >
-              🎁→
-            </span>
-            <img src={ben} alt="ben" height="100" width="100" />
-          </div>
-
-          <div className="list-item">
-            <img src={kylee} alt="kylee" height="100" width="100" />
-            <span
-              style={{ fontSize: "40px" }}
-              role="img"
-              aria-label="gives gift to"
-            >
-              🎁→
-            </span>
-            <img src={mike} alt="mike" height="100" width="100" />
-          </div>
-        </div>
+  return (
+    <div className="App">
+      <div className="list-header">Christmas Assignments</div>
+      <div className="list-select">
+        <button onClick={() => setListSelected("adults")}>Adults</button>
+        <button onClick={() => setListSelected("kids")}>Kids</button>
       </div>
-    );
-  }
+      <div className="list">
+        {(listSelected === "adults" ? adultMatchups : kidMatchups).map(
+          (obj) => (
+            <div className="list-item">
+              <img
+                src={obj.giveFrom.img}
+                alt={obj.giveFrom.name}
+                height="100"
+                width="100"
+              />
+              <span
+                style={{ fontSize: "40px" }}
+                role="img"
+                aria-label="gives gift to"
+              >
+                🎁 →
+              </span>
+              <img
+                src={obj.giveTo.img}
+                alt={obj.giveTo.name}
+                height="100"
+                width="100"
+              />
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
 }
-
-export default App;
